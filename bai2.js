6e8196ed0b5b808b189f10a24e36a578d7944e5c
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-10-08 15:12:24 +0700

    add 6 file

---
+++ bai2.js
@@ -0,0 +1,4 @@
+let width=20
+let height=20
+let area=width*height
+document.write('Area='+area);
---
+++ bai3.js
@@ -0,0 +1,7 @@
+let a=prompt("Nhap a=", "");
+let b=prompt("Nhap b=", "");
+if(a%b==0){
+    alert("A chia het cho b")
+}else{
+    alert("a khong chia het cho b")
+}
